87592ee57f3ddbdf9adf2908ea36470ee1049e08
Andy Bond <andybond87@gmail.com>
2018-06-18 13:13:59 +0100

    First working version

---
+++ src/minimap2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# -e = exit on error; -x = output each line that is executed to log; -o pipefail = throw an error if there's an error in pipeline
+set -e -x -o pipefail
+
+main() {
+
+    # Download fastq and reference genome
+    dx-download-all-inputs 
+
+    # Create output directories
+    mkdir -p $HOME/out/bam/output/
+    mkdir -p $HOME/out/bai/output/
+
+    # Extract minimap2
+    tar -jxvf minimap2-2.8_x64-linux.tar.bz2
+
+    # Run minimap2 using Oxford Nanopore settings
+    ./minimap2-2.8_x64-linux/minimap2 -ax map-ont -t 4 $reference_genome_path $fastq_path > ${fastq_prefix}.sam
+
+    # sort and index with samtools
+    dx-docker run -v ~:/data quay.io/biocontainers/samtools:1.5--2 /bin/bash -c "samtools view -b -S /data/${fastq_prefix}.sam | \
+    samtools sort - -o /data/${fastq_prefix}.bam && samtools index /data/${fastq_prefix}.bam"
+
+    # Move bam and bam index to output directories
+    sudo mv $HOME/${fastq_prefix}.bam $HOME/out/bam/output/
+    sudo mv $HOME/${fastq_prefix}.bam.bai $HOME/out/bai/output/
+
+    # Upload output
+    dx-upload-all-outputs
+}
